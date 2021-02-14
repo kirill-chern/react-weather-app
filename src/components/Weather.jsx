d8d361c6f424e4fec4e52e22ba7465ba27ed0028
Kyrylo Chernyavskyi <kiril.chwork@gmail.com>
2021-02-14 17:21:22 +0200

    creation Weather component and its first version

---
+++ src/components/Weather.jsx
@@ -0,0 +1,11 @@
+function Weather() {
+    return (
+        <div className="container">
+            <div className="cards">
+                <h1>Weather app</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Weather;
